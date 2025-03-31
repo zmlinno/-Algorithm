332ea437d46d9773a4ee46937853c393f667fdc3
zmlinno <131747526+zmlinno@users.noreply.github.com>
2025-03-31 23:34:15 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	cout << endl;
+
+}
